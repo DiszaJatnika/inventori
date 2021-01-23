72b39aa9da6bf6a90e03db0e08736fd33f4a5ed5
diszajatnika <diszaid@gmail.com>
2021-01-23 17:52:20 +0700

    asjdkajsda

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -17,7 +17,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 		font: 13px/20px normal Helvetica, Arial, sans-serif;
 		color: #4F5155;
 	}
-
+     
 	a {
 		color: #003399;
 		background-color: transparent;
